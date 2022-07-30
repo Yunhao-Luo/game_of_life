11fe21ba827b3144588c29f327deb64ec9046b69
Yunhao Luo <yunhaoluo@cs.ucsb.edu>
2022-07-30 16:51:50 -0700

    added restart button

--- test.py
+++ test.py
@@ -1,34 +1,7 @@
-import tkinter as tk
-from tkinter import ttk
-import time
-
-root = tk.Tk()
-parent = tk.Frame(root)
-
-buttonNames = ['numberOne','numberTwo','happyButton']
-buttonDic = {}
-buttonColors = {}
-
-def change_color(name):
-    buttonColors[name].set("red")
-    buttonDic[name].config(background=buttonColors[name].get())
-
-for name in buttonNames:
-    buttonColors[name] = tk.StringVar()
-    buttonColors[name].set("blue")
-    buttonDic[name] = ttk.Button(
-            parent,
-            text = name,
-            width = 20,
-            background = buttonColors[name].get(),
-            command= lambda passName=name: change_color(passName)
-            )
-
-parent.grid(row=0,column=0)
-for i,name in enumerate(buttonNames):
-    buttonDic[name].grid(row=i,column=0)
-
-time.sleep(1)
-change_color('numberOne')
-
-root.mainloop()
+from tkinter import *
+parent = Tk()
+parent.geometry('500x500')
+button1 = Button(parent, text = 'click me!', fg='red', highlightbackground='yellow' )
+button1.pack()
+print(button1["highlightbackground"])
+parent.mainloop()
--- ui.py
+++ ui.py
@@ -10,27 +10,62 @@ class UI(tk.Tk):
         self.title("Game of Life")
         self.geometry("800x600")
         self.bt_list = []
-        self.game = Game(9, 13)
+        self.row = 25
+        self.col = 25
+        self.game = Game(25, 25)
         self.game_board = self.game.ret_board()
         self.count = 0
+        self.first_r = True
+        self.current = []
+        self.abort = False
 
     def board(self):
-        for i in range(len(self.game_board)):
+        for i in range(self.row):
             self.bt_list.append([])
             row = self.bt_list[-1]
-            for j in range(len(self.game_board[0])):
-                if self.game_board[i][j] == 0:
-                    cell = tk.Button(self, width=5)
-                else:
-                    cell = tk.Button(self, width=5, highlightbackground="grey")
+            for j in range(self.col):
+                cell = tk.Button(self, width=3, highlightbackground="white", command=lambda r=i, c=j:self.select(r, c))
                 row.append(cell)
                 cell.grid(row = i, column= j)
-        tk.Button(self, text="start", command=self.change, width=5).grid(row=len(self.game_board), column=0)
-        
+        tk.Button(self, text="start", command=self.change, width=5).grid(row=self.row, column=0, columnspan=2)
+        tk.Button(self, text="restart", command=self.restart, width=5).grid(row=self.row, column=2, columnspan=2)
     
+    def init_board(self):
+        res = []
+        for i in range(len(self.bt_list)):
+            res.append([])
+            temp = res[-1]
+            for j in range(len(self.bt_list[i])):
+                if self.bt_list[i][j]['highlightbackground'] == "grey": temp.append(1)
+                else: temp.append(0)
+        return res
+
+    def select(self, r, c):
+        if self.bt_list[r][c]["highlightbackground"] == "white":
+            self.bt_list[r][c].configure(highlightbackground='grey')
+        else:
+            self.bt_list[r][c].configure(highlightbackground='white')
+        self.abort = False
+    
+    def restart(self):
+        self.abort = True
+        self.clear_board()
+        self.init_board()
+        self.game_board = []
+        self.first_r = True
+    
+    def clear_board(self):
+        for r in range(len(self.bt_list)):
+            for c in range(len(self.bt_list[r])):
+                self.bt_list[r][c].configure(highlightbackground='white')
+
     def change(self):
-        print("called")
-        print(self.count)
+        if self.abort:
+            return
+        if self.first_r:
+            self.game.read_baord(self.init_board())
+            self.first_r = False
+        print(f"round: {self.count}")
         self.count+=1
         self.game_board = self.game.new_round()
         for r in range(len(self.game_board)):
@@ -39,6 +74,7 @@ class UI(tk.Tk):
                     self.bt_list[r][c].configure(highlightbackground='grey')
                 else:
                     self.bt_list[r][c].configure(highlightbackground='white')
+
         self.bt_list[0][0].after(200, # milliseconds
                         self.change)
 
