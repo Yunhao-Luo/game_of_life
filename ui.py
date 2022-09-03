ae538e4151202fc9e697ecd5e6955f23013b1203
Yunhao Luo <yunhaoluo@cs.ucsb.edu>
2022-09-02 21:46:14 -0700

    added pause and living cells display

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
+from cgitb import text
 import tkinter as tk
 import time
+from typing import Text
 
 from game import *
 
@@ -10,16 +12,19 @@ class UI(tk.Tk):
         self.title("Game of Life")
         self.geometry("800x600")
         self.w, self.h = self.winfo_screenwidth(), self.winfo_screenheight()
-        self.geometry("%dx%d+0+0" % (0.995 * self.w, 0.9 * self.h))
+        width = 950
+        height = 500
+        self.geometry("%dx%d+0+0" % (width, height))
         self.bt_list = []
-        self.row = 33
-        self.col = 46
+        self.row = 21
+        self.col = 30
         self.game = Game(25, 25)
         self.game_board = self.game.ret_board()
         self.count = 0
         self.first_r = True
         self.current = []
         self.abort = False
+        self.liveCells = 0
 
     def board(self):
         for i in range(self.row):
@@ -31,6 +36,10 @@ class UI(tk.Tk):
                 cell.grid(row = i, column= j)
         tk.Button(self, text="start", command=self.change, width=5).grid(row=self.row, column=0, columnspan=2)
         tk.Button(self, text="restart", command=self.restart, width=5).grid(row=self.row, column=2, columnspan=2)
+        self.pauseBt = tk.Button(self, text="pause", command=self.pause, width=5)
+        self.pauseBt.grid(row=self.row, column=4, columnspan=2)
+        self.liveCount = tk.Label(self, text="There are 0 living cell", width=15)
+        self.liveCount.grid(row=self.row, column=6, columnspan=15)
     
     def init_board(self):
         res = []
@@ -45,21 +54,31 @@ class UI(tk.Tk):
     def select(self, r, c):
         if self.bt_list[r][c]["highlightbackground"] == "white":
             self.bt_list[r][c].configure(highlightbackground='grey')
+            self.game_board[r][c] = 1
         else:
             self.bt_list[r][c].configure(highlightbackground='white')
-        self.abort = False
+            self.game_board[r][c] = 0
     
     def restart(self):
-        self.abort = True
+        self.abort = False
         self.clear_board()
         self.init_board()
         self.game_board = []
         self.first_r = True
+        self.liveCount.config(text=f"There are {0} living cell")
     
     def clear_board(self):
         for r in range(len(self.bt_list)):
             for c in range(len(self.bt_list[r])):
                 self.bt_list[r][c].configure(highlightbackground='white')
+    
+    def pause(self):
+        self.abort = not self.abort
+        if self.abort == True:
+            self.pauseBt.config(text="resume")
+        else:
+            self.pauseBt.config(text="pause")
+            self.change()
 
     def change(self):
         if self.abort:
@@ -70,12 +89,16 @@ class UI(tk.Tk):
         print(f"round: {self.count}")
         self.count+=1
         self.game_board = self.game.new_round()
+        self.liveCells = 0
         for r in range(len(self.game_board)):
             for c in range(len(self.game_board[r])):
                 if self.game_board[r][c] == 1:
                     self.bt_list[r][c].configure(highlightbackground='grey')
+                    self.liveCells += 1
                 else:
                     self.bt_list[r][c].configure(highlightbackground='white')
+
+        self.liveCount.config(text=f"There are {self.liveCells} living cells")
 
         self.bt_list[0][0].after(200, # milliseconds
                         self.change)
